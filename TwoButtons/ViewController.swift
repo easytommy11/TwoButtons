8e42a64fe385b8a0e3f93193188de447d927e17c
Tomiwa Babalola <tbabalola004@gmail.com>
2021-10-22 09:35:02 -0400

    nwlknlkds

--- TwoButtons/ViewController.swift
+++ TwoButtons/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var textfield: UITextField!
+    
+    
+    @IBOutlet var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ctbutton(_ sender: Any) {
+        label.text = ""
+    }
+    
+    @IBAction func stbutton(_ sender: Any) {
+        label.text = textfield.text
+    }
 }
 
